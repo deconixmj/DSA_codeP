2d9c4fc41f0aed9d2351ece8da6327ff19384da8
deconixmj <41852525+deconixmj@users.noreply.github.com>
2023-04-06 21:19:42 +0530

    Update BinaryGap.py

--- Codility/BinaryGap.py
+++ Codility/BinaryGap.py
@@ -1,1 +1,16 @@
-
+ef solution(N):
+    #convert the number into binary
+    # L=[]
+    k = bin(N)[2:]
+    # str1=str(binary)
+    # print(str1)
+    countgap=0
+    maxgap=0
+    for i in k:
+        if int(i)==0:
+            countgap+=1
+        elif int(i)==1:
+            maxgap=max(maxgap,countgap)
+            countgap=0
+    # print(L)
+    return maxgap
