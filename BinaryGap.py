b1acc9b3d7ca1cbcf7765823441d46600c4bddee
deconixmj <41852525+deconixmj@users.noreply.github.com>
2023-04-06 21:06:50 +0530

    Add files via upload

---
+++ BinaryGap.py
@@ -0,0 +1,21 @@
+def solution(N):
+    #convert the number into binary
+    # L=[]
+    k = bin(N)[2:]
+    # str1=str(binary)
+    # print(str1)
+    countgap=0
+    maxgap=0
+    for i in k:
+        if int(i)==0:
+            countgap+=1
+        elif int(i)==1:
+            maxgap=max(maxgap,countgap)
+            countgap=0
+    # print(L)
+    return maxgap
+        
+
+    # print(binary)
+    
+print(solution(1041))
